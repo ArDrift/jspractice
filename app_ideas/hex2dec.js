6c01698eb08dfdcedc307d919d35e1b1bf032b3a
ArDrift <ardrift@gmail.com>
2019-12-17 17:07:57 +0100

    Add hex2dec.js

---
+++ app_ideas/hex2dec.js
@@ -0,0 +1,40 @@
+function hex2dec(hexInput) {
+  const hexDigits = hexInput.split("");
+  let result = 0;
+  for (let i = 0; i < hexDigits.length; i++) {
+    let powerCounter = hexDigits.length - 1 - i;
+    // if digit is decimal number
+    if (isNaN(parseInt(hexDigits[i])) === false ) {
+      result += hexDigits[i] * Math.pow(16, powerCounter);
+    }
+    else {
+      switch (hexDigits[i]) {
+        case "A":
+          result += 10 * Math.pow(16, powerCounter);
+        break;
+        case "B":
+          result += 11 * Math.pow(16, powerCounter);
+        break;
+        case "C":
+          result += 12 * Math.pow(16, powerCounter);
+        break;
+        case "D":
+          result += 13 * Math.pow(16, powerCounter);
+        break;
+        case "E":
+          result += 14 * Math.pow(16, powerCounter);
+        break;
+        case "F":
+          result += 15 * Math.pow(16, powerCounter);
+        break;
+        default:
+          console.log("Given input is not a hexadecimal number!");
+          return "Given input is not a hexadecimal number!";
+      }
+    }
+  }
+  console.log(result);
+  return result;
+}
+
+hex2dec("100");
