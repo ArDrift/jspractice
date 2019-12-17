7725db520eab264b5325b17257afb2f5f828b81a
ArDrift <ardrift@gmail.com>
2019-12-17 14:41:40 +0100

    Add bin2dec.js

---
+++ app_ideas/bin2dec.js
@@ -0,0 +1,15 @@
+function bin2dec(binInput) {
+  const binDigits = binInput.split("");
+  let result = 0;
+  for (let i = 0; i < binDigits.length; i++) {
+    let powerCounter = binDigits.length - 1 - i;
+    // if digit is 1 add its decimal value to result
+    if (binDigits[i] === "1") {
+      result += Math.pow(2, powerCounter);
+    }
+  }
+  console.log(result);
+  return result;
+}
+
+bin2dec("00000001");
