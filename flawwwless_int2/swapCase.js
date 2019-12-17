b13bd662db7ee25f9b2f943bebdb46a0acf96fb9
ArDrift <ardrift@gmail.com>
2019-12-17 11:44:52 +0100

    Completed swapCase, flawwwless(ly) done

--- flawwwless_int2/swapCase.js
+++ flawwwless_int2/swapCase.js
@@ -1,12 +1,19 @@
 function swapCase(str) {
-  const chars = str.split("");
+  // split input to characters
+  let chars = str.split("");
   for (let i = 0; i < chars.length; i++) {
-    // if current char is already uppercase
-    if (chars[i] === chars[i].toUpperCase) {
-      chars[i] = chars[i].toLowerCase;
+    // if current char is already upper case
+    if (chars[i] === chars[i].toUpperCase()) {
+      chars[i] = chars[i].toLowerCase();
     }
-    else if (chars[i] === chars[i].toLowerCase) {
-      chars[i] = chars[i].toUpperCase;
+    // if current char is already lower case
+    else if (chars[i] === chars[i].toLowerCase()) {
+      chars[i] = chars[i].toUpperCase();
     }
   }
+  chars = chars.join("");
+  console.log(chars);
+  return chars;
 }
+
+swapCase("Hello World");
