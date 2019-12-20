fede14ea58d78aa426a4fe9f6c18c94cba20ecc3
ArDrift <ardrift@gmail.com>
2019-12-20 16:54:26 +0100

    Initial working frontend for bin2dec

--- app_ideas/bin2dec.js
+++ app_ideas/bin2dec.js
@@ -1,6 +1,9 @@
-function bin2dec(binInput) {
-  const binDigits = binInput.split("");
+function bin2dec() {
+  const inputField = document.getElementById("binInput");
+  const binInput = inputField.value;
+  const binDigits = binInput.toString().split("");
   let result = 0;
+  const resultField = document.getElementById("result");
   for (let i = 0; i < binDigits.length; i++) {
     let powerCounter = binDigits.length - 1 - i;
     // if digit is 1 add its decimal value to result
@@ -10,11 +13,10 @@ function bin2dec(binInput) {
     // if digit is neither a 1 nor a 0, e.g. not binary
     else if (binDigits[i] !== "0") {
       console.log("Error, input is not a binary number!");
-      return "Error, input is not a binary number!";
+      result = "Error, input is not a binary number!";
     }
   }
   console.log(result);
+  resultField.value = result;
   return result;
 }
-
-bin2dec("00000001");
