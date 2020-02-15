b89d409988600a12fcf5d872d9583e71c8a19df2
ArDrift <ardrift@gmail.com>
2020-02-15 17:33:14 +0100

    [WIP] Start dec2bin from scratch

--- app_ideas/dec2bin.js
+++ app_ideas/dec2bin.js
@@ -11,23 +11,26 @@ function dec2bin(decInput) {
     powerCounter = byteNum * 8 - 1;
     console.log("Power counter start: " + powerCounter);
     for (powerCounter; powerCounter >= 0; powerCounter--) {
-      switch (true) {
-        case (decNum - Math.pow(2, powerCounter) > 0):
-          decNum = decNum - Math.pow(2, powerCounter);
-          binNum.unshift(1);
-          console.log(binNum);
-          break;
-        case 0:
-          console.log(binNum);
-          break;
-        case (decNum - Math.pow(2, powerCounter) < 0):
-          binNum.unshift(0);
-          console.log(binNum);
-          break;
+      // If result is more than zero, bit is 1
+      if (decNum - Math.pow(2, powerCounter) > 0) {
+        decNum = decNum - Math.pow(2, powerCounter);
+        //console.log(decNum);
       }
-      return binNum;
+      // If result is less than zero, bit is 0
+      else if (decNum - Math.pow(2, powerCounter) < 0) {
+        binNum.push(0);
+        //console.log(decNum);
+      }
+      // If result is 0, final result is computed
+      else {
+        binNum.push(1);
+        //console.log(binNum);
+      }
     }
+    binNum = binNum.join("");
+    console.log("Result is: " +binNum);
+    return binNum;
   }
 }
 
-dec2bin(255);
+dec2bin(6);
