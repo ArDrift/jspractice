de481f6beec331320e62735dcddb7e37e06f637e
Arnold Schelb <ardrift@gmail.com>
2020-02-17 23:10:05 +0100

    Clear the clutter

--- app_ideas/dec2bin.js
+++ app_ideas/dec2bin.js
@@ -8,23 +8,24 @@ function dec2bin(decInput) {
 
   // Byte counter
   bytesNum += Math.floor(decInput / 256);
-  console.log("Number of bytes: " + bytesNum);
+  // console.log("Number of bytes: " + bytesNum);
   // Start from power n
   powerCounter = bytesNum * 8 - 1;
-  console.log("Start subtracting from: " + Math.pow(2, powerCounter));
+  // console.log("Start subtracting from: " + Math.pow(2, powerCounter));
   for (bytesNum; bytesNum >= 1; bytesNum--) {
     for (powerCounter; powerCounter >= 0; powerCounter--) {
       if (decNum - Math.pow(2, powerCounter) >= 0) {
         decNum = decNum - Math.pow(2, powerCounter);
         binNum.push(1);
       }
-      else {
+      // Don't add unnecessary zeros to the beginning
+      else if (binNum.length !== 0) {
         binNum.push(0);
       }
     }
   }
   binOut = binNum.join("");
-  console.log("Result is: " + binOut);
+  // console.log("Result is: " + binOut);
   return binOut;
 }
 
